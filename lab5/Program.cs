eb48af9778ba9e53cfafe7afd3924df6698e59e9
Dmitry Kalinin <mitya.kalinin1@gmail.com>
2022-09-02 15:08:32 +0300

    init

---
+++ lab5/Program.cs
@@ -0,0 +1,12 @@
+﻿// подзадача 2
+for (int i = 10; i < 100; i++)
+{
+    for (int j = 10; j < 100; j++)
+    {
+        int s = (i * 100 + j) % (i * j);
+        if (s == 0) 
+        {
+            Console.WriteLine(String.Format("Num1: {0}, num2:{1}", i, j));
+        }
+    }
+}
