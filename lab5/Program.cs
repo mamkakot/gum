10a28541a9163c187639d2f5c1ae09b955bc9486
Dmitry Kalinin <mitya.kalinin1@gmail.com>
2022-09-02 23:43:42 +0700

    added first task

--- lab5/Program.cs
+++ lab5/Program.cs
@@ -1,12 +1,32 @@
-﻿// подзадача 2
-for (int i = 10; i < 100; i++)
+﻿// подзадача 1
+int ConvertToBase10(int n, int init_base)
+{
+    int mod = 0, // остаток от деления
+        count = 0, // счётчик разрядов
+        result = 0; // результат вычисления функции
+    
+    while (n > 0)
+    {
+        mod = n % 10;
+        int s = Convert.ToInt32(Math.Pow(init_base, count));
+        result += mod * s;
+        count++;
+        n /= 10; // переход к высшему разряду
+    }
+    return result;
+}
+
+Console.WriteLine(ConvertToBase10(11, 5)); 
+
+// подзадача 2
+for (int i = 10; i < 100; i++) // цикл по всем двузначным числам
 {
     for (int j = 10; j < 100; j++)
     {
-        int s = (i * 100 + j) % (i * j);
+        int s = (i * 100 + j) % (i * j); // остаток от деления четырёхзначного числа на произведение
         if (s == 0) 
         {
-            Console.WriteLine(String.Format("Num1: {0}, num2:{1}", i, j));
+            Console.WriteLine(String.Format("first number: {0}, second number: {1}", i, j));
         }
     }
 }
