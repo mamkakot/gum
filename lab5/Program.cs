6d12bd8909e4c2f39c27759686fcc549544567f0
Dmitry Kalinin <mitya.kalinin1@gmail.com>
2022-09-03 06:27:24 +0300

    added readme, changed first task

--- lab5/Program.cs
+++ lab5/Program.cs
@@ -4,7 +4,7 @@ int ConvertToBase10(int n, int init_base)
     int mod = 0, // остаток от деления
         count = 0, // счётчик разрядов
         result = 0; // результат вычисления функции
-    
+
     while (n > 0)
     {
         mod = n % 10;
@@ -16,7 +16,10 @@ int ConvertToBase10(int n, int init_base)
     return result;
 }
 
-Console.WriteLine(ConvertToBase10(11, 5)); 
+Console.WriteLine("Введите последовательно через пробел два числа: число, которое будет преобразовано в десятичную систему, и основание системы счисления введённого числа:");
+string[] str = Console.ReadLine().Split(' ');
+int n = Convert.ToInt32(str[0]), b = Convert.ToInt32(str[1]);
+Console.WriteLine($"Число {n} с основанием системы счисления {b} в десятичной системе счисления: {ConvertToBase10(n, b)}");
 
 // подзадача 2
 for (int i = 10; i < 100; i++) // цикл по всем двузначным числам
@@ -24,9 +27,9 @@ for (int i = 10; i < 100; i++) // цикл по всем двузначным ч
     for (int j = 10; j < 100; j++)
     {
         int s = (i * 100 + j) % (i * j); // остаток от деления четырёхзначного числа на произведение
-        if (s == 0) 
+        if (s == 0)
         {
-            Console.WriteLine(String.Format("first number: {0}, second number: {1}", i, j));
+            Console.WriteLine(String.Format("Первое двузначное число: {0}, второе двузначное число: {1}", i, j));
         }
     }
 }
