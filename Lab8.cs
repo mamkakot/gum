c1bf1234dad90dc81b4b00fdb79a35908ad250e5
Dmitry Kalinin <mitya.kalinin1@gmail.com>
2022-09-08 17:34:46 +0300

    lab8 done

--- Lab8.cs
+++ Lab8.cs
@@ -1,39 +1,47 @@
+using System.Text.RegularExpressions;
 public class Lab8
 {
-    private string[][] array;
+    private string[] array;
     public Lab8()
     {
-        this.array = System.IO.File.ReadAllLines("./input.txt").Select(s => s.ToLower().Split(' ')).ToArray();
+        var rx = new Regex(@"\s+", RegexOptions.Compiled);
+        var str = System.IO.File.ReadAllText("./input.txt");
+        this.array = rx.Split(str).Select(s => s.ToLower()).ToArray();
         SortArray();
-        Console.WriteLine(string.Join('\n', array.Select(s => string.Join(' ', s))));
+        Console.WriteLine("Введите название файла: ");
+        var fileName = Console.ReadLine();
+        if (string.IsNullOrEmpty(fileName)) fileName = "output.txt";
+        using (StreamWriter w = File.CreateText(fileName))
+        {
+            w.Write(string.Join(' ', array));
+        }
     }
 
-    // TODO: запилить нормальную сортировку
     private void SortArray()
     {
-        int[][] byteCodes = new int[array.Length][];
         string temp;
-
-        foreach (var strArr in array)
+        for (int write = 0; write < array.Length; write++)
         {
-            for (int write = 0; write < strArr.Length; write++)
+            for (int sort = 0; sort < (array.Length - 1); sort++)
             {
-                for (int sort = 0; sort < strArr[write].Length - 1; sort++)
+                if (CompareStrings(array[sort], array[sort + 1]) > 0)
                 {
-                    if ((int)strArr[sort][0] > (int)strArr[sort + 1][0])
-                    {
-                        temp = strArr[sort + 1];
-                        strArr[sort + 1] = strArr[sort];
-                        strArr[sort] = temp;
-                    }
+                    temp = array[sort + 1];
+                    array[sort + 1] = array[sort];
+                    array[sort] = temp;
                 }
             }
         }
-
-
     }
-    private void Swap(int index1, int index2)
+    private int CompareStrings(string str1, string str2)
     {
+        var l = str1.Length < str2.Length ? str1.Length : str2.Length;
+        for (var i = 0; i < l; i++)
+        {
+            if (str1[i] < str2[i]) return -1;
+            if (str1[i] > str2[i]) return 1;
+        }
 
+        return str1.Length > str2.Length ? 1 : 0;
     }
 }
